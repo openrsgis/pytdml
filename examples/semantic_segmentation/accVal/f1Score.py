a14f908e19c1fbb7da8f649a6626dce786dd2a21
sgby <sgby@whu.edu.cn>
2022-04-22 18:59:05 +0800

    Update

---
+++ examples/semantic_segmentation/accVal/f1Score.py
@@ -0,0 +1,116 @@
+"""
+    f1 to F1 score is used for evaluation the performance of the model
+"""
+import os
+import cv2 as cv
+
+
+def averagenum(num):
+    nsum = 0
+    for i in range(len(num)):
+        nsum += num[i]
+    return nsum / len(num)
+
+
+def sumnum(num):
+    nsum = 0
+    for i in range(len(num)):
+        nsum += num[i]
+    return nsum
+
+
+"""Calculate precision, recall, f1-score of a single category, except 0 category"""
+
+
+def cal_p_r_each(pre_img, gt_img, class_type):
+    TP = 0
+    TN = 0
+    FN = 0
+    FP = 0
+    precision = 0
+    for i in range(pre_img.shape[0]):
+        for j in range(pre_img.shape[1]):
+            if gt_img[i][j] == 0:
+                break
+            else:
+                if (pre_img[i][j] == class_type and gt_img[i][j] == class_type):
+                    TP += 1
+                elif (not pre_img[i][j] == class_type and not gt_img[i][j] == class_type):
+                    TN += 1
+                elif (not pre_img[i][j] == class_type and gt_img[i][j] == class_type):
+                    FN += 1
+                elif (pre_img[i][j] == class_type and not gt_img[i][j] == class_type):
+                    FP += 1
+    return TP, TN, FN, FP
+
+
+"""Calculate precision, recall, f1-score for all categories, except 0"""
+
+
+def get_all_p_r(pre_img, gt_img):
+    list_TP = []
+    list_TN = []
+    list_FN = []
+    list_FP = []
+    for i in range(1, 6):
+        TP, TN, FN, FP = cal_p_r_each(pre_img, gt_img, i)
+        list_TP.append(TP)
+        print(TP)
+        list_TN.append(TN)
+        print(TN)
+        list_FN.append(FN)
+        list_FP.append(FP)
+
+    total_TP = sumnum(list_TP)
+    total_TN = sumnum(list_TN)
+    total_FN = sumnum(list_FN)
+    total_FP = sumnum(list_FP)
+    total_p = 0
+    total_r = 0
+    micro_f = 0
+    try:
+        total_p = total_TP / (total_TP + total_FP)
+        total_r = total_TN / (total_TN + total_FN)
+        micro_f = 2 * total_p * total_r / (total_p + total_r)
+    except Exception as e:
+        print(e)
+
+    return total_p, total_r, micro_f
+
+
+def import_img(img_path):
+    img_path_list = os.listdir(img_path)
+    img_path_list.sort()
+    img_list = []
+    for i in range(len(img_path_list)):
+        img = cv.imread(img_path + img_path_list[i], flags=cv.IMREAD_UNCHANGED)
+        t = img.shape
+        img_list.append(img)
+    return img_list
+
+
+if __name__ == '__main__':
+    pre_path = "/home/pySampleCube/RSTask/DeepLabV32/result"
+    gt_path = "/home/RSdata/LandUse/GID5class/test/label"
+    pre_img_list = import_img(pre_path)
+    gt_img_list = import_img(gt_path)
+    precision = []
+    recall = []
+    f1Score = []
+    for i in range(len(pre_img_list)):
+        print(str(i + 1))
+        total_p, total_r, micro_f = get_all_p_r(pre_img_list[i], gt_img_list[i])
+        precision.append(total_p)
+        recall.append(total_r)
+        f1Score.append(micro_f)
+
+    m_precision = averagenum(precision)
+    print("precision: " + str(m_precision))
+
+    m_recall = averagenum(recall)
+    print("recall: " + str(m_recall))
+
+    m_f1Score = averagenum(f1Score)
+    print("f1Score: " + str(m_f1Score))
+
+    print("finish!")
---
+++ pytdml/tdml_image_crop.py
@@ -0,0 +1,89 @@
+# ------------------------------------------------------------------------------
+#
+# Project: pytdml
+#
+# ------------------------------------------------------------------------------
+#
+# ------------------------------------------------------------------------------
+import json
+import os
+import sys
+
+import cv2
+
+from pytdml.io import read_from_json
+from pytdml.type import EOTrainingDataset, EOTrainingData, PixelLabel
+from pytdml.utils import remove_empty
+
+
+def td_image_crop(td: EOTrainingDataset, save_tdml_path: str, save_crop_dir: str, sub_size: int):
+    td_dict = td.to_dict()
+    td_list = td_dict['data']
+    new_td_list = []
+    index = 0
+    for d in td_list:
+        label_type = d['labels'][0]['type']
+        if label_type == 'PixelLabel':
+            image_url = d['dataUrl']
+            label_url = d['labels'][0]['imageUrl']
+            image_dir = os.path.join(save_crop_dir, "images")
+            label_dir = os.path.join(save_crop_dir, "labels")
+            if not os.path.isdir(image_dir):
+                os.makedirs(image_dir)
+            if not os.path.isdir(label_dir):
+                os.makedirs(label_dir)
+            crop_image_list = image_crop(image_url, image_dir, sub_size)
+            crop_label_list = image_crop(label_url, label_dir, sub_size)
+            for crop_image_url, crop_label_url in zip(crop_image_list, crop_label_list):
+                new_d = EOTrainingData(
+                    id=str(index),
+                    labels=[PixelLabel(image_url=crop_label_url)],
+                    data_url=crop_image_url,
+                )
+                index = index + 1
+                new_td_list.append(new_d.to_dict())
+    td_dict['data'] = new_td_list
+    with open(save_tdml_path, "w") as f:
+        json.dump(remove_empty(td_dict), f, indent=4)
+
+
+def image_crop(data_url, save_dir, sub_size):
+    """
+    image crop form data url
+    """
+    image = cv2.imread(data_url)
+    image_name = os.path.basename(data_url)
+    h = image.shape[0]
+    w = image.shape[1]
+    crop_image_path_list = []
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+    if h > sub_size & (h > sub_size):
+        for i in range(int(w / sub_size)):
+            for j in range(int(h / sub_size)):
+                crop_image_path = os.path.join(save_dir, str(i) + str(j) + image_name)
+                crop_image = image[j * sub_size: sub_size * (j + 1), i * sub_size: sub_size * (i + 1)]
+                cv2.imwrite(crop_image_path, crop_image)
+                crop_image_path_list.append(crop_image_path)
+    return crop_image_path_list
+
+
+def main(argv=None):
+    if len(argv) < 4:
+        print("Please set parameters "
+              "<*input tdml json file path> "
+              "<*save path of new tdml json file> "
+              "<*save dir of result images and labels> "
+              "<*crop size>")
+        return
+    tdml_path = argv[1]
+    save_tdml_path = argv[2]
+    save_crop_dir = argv[3]
+    sub_size = int(argv[4])
+    training_datasets = read_from_json(tdml_path)
+    if training_datasets:
+        td_image_crop(training_datasets, save_tdml_path, save_crop_dir, sub_size)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
