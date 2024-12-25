3fbe4467119546a825e10d8d5e6236cd4eaeb36f
relifest <3380383714@qq.com>
2024-12-25 17:44:51 +0800

    Check and update the S3_deader and coco_converter functions

--- tests/test_tdml_io.py
+++ tests/test_tdml_io.py
@@ -12,6 +12,9 @@ from pytdml.io.tdml_readers import read_from_json
 from pytdml.io.stac_converter import convert_stac_to_tdml
 
 base_url = "https://raw.githubusercontent.com/opengeospatial/TrainingDML-AI_SWG/main/schemas/1.0/json_schema/{}.json"
+remote_schema_url = base_url.format("ai_eoTrainingDataset")
+response = requests.get(remote_schema_url)
+remote_schema = response.json()
 
 def test_read_and_write():
     tdml_path = r"tests/data/json/WHU-building.json"
@@ -31,23 +34,14 @@ def test_yaml_to_eo_tdml():
 def test_convert_stac_to_tdml():
     stac_file_path = r"tests/data/coco/collection.json"
     td = convert_stac_to_tdml(stac_file_path)
-    remote_schema_url = base_url.format("ai_eoTrainingDataset")
-    response = requests.get(remote_schema_url)
-    remote_schema = response.json()
     jsonschema.validate(instance=td.to_dict(), schema=remote_schema)
 
 def test_coco_converter_Panoptic_Segmentation():
     coco_file_path = r"tests/data/coco/panoptic_val2017.json"
     td = convert_coco_to_tdml(coco_file_path)
-    remote_schema_url = base_url.format("ai_eoTrainingDataset")
-    response = requests.get(remote_schema_url)
-    remote_schema = response.json()
     jsonschema.validate(instance=td.to_dict(), schema=remote_schema)
 
 def test_coco_converter_Image_Captioning():
     coco_file_path = r"tests/data/stac/captions_val2014.json"
     td = convert_coco_to_tdml(coco_file_path)
-    remote_schema_url = base_url.format("ai_eoTrainingDataset")
-    response = requests.get(remote_schema_url)
-    remote_schema = response.json()
     jsonschema.validate(instance=td.to_dict(), schema=remote_schema)
